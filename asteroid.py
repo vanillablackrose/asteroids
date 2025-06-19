38458aa00774188c82a20406155105f6129b2411
vanillablackrose <vanillablackrose@gmail.com>
2025-06-19 12:37:19 -0400

    Completed the asteroids lesson

---
+++ asteroid.py
@@ -0,0 +1,28 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+import random
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+            v1 = self.velocity.rotate(random_angle) * 1.2
+            v2 = self.velocity.rotate(-random_angle) * 1.2
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid1.velocity = v1
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid2.velocity = v2
--- main.py
+++ main.py
@@ -3,18 +3,63 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+import sys
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt_var = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    player_ship = Player(x, y)
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")    
+                    
+        screen.fill("black") 
+
+        for obj in updatable:
+            obj.update(dt_var)
+           
+        for obj in asteroids:
+            if obj.check_for_collision(player_ship):
+                print("Game over!")
+                sys.exit()
+
+            for bullet in shots:
+                if obj.check_for_collision(bullet):
+                    bullet.kill()
+                    obj.split()
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        delta_time = game_clock.tick(60.0)
+        dt_var = delta_time/1000
+        
 
 if __name__ == "__main__":
     main()
